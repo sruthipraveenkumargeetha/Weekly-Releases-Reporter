183830b7a153007978c3220d4f8163d1f9164cea
SruthiPraveenKGeetha <spg003@ucsd.edu>
2023-06-02 01:26:25 -0700

    Added the curl command to fetch movie releases data from API endpoint

--- release-report.sh
+++ release-report.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
 # Fetch release data using curl
-release_data=$(curl -s "https://api.example.com/weather")  # Replace with the actual API endpoint
+# Calculate the last Friday
+start_date=$(date -d "last Friday" +"%Y-%m-%d")
+
+# Calculate the following Friday
+end_date=$(date -d "$start_date + 7 days" +"%Y-%m-%d")
+
+# Make the API request using curl
+release_data=$(curl -s --request GET \
+  --url "https://api.themoviedb.org/3/discover/movie?api_key=24bdf946bd1af77401fdbc8b97fe0d4b&primary_release_date.gte=$start_date&primary_release_date.lte=$end_date")
 
 # Email configuration
 recipient="sruthi17pg@gmail.com"  # Replace with your email address
